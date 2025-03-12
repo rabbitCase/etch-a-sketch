2d94407c13c694b392249caf98ceeeae6a22ac9c
rabbitCase <gnair3004@gmail.com>
2025-03-12 14:32:34 +0530

    added toolbar, cursor, improved aesthetics, refactored code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded',() =>{
     const gridContainer = document.getElementsByClassName('grid-container')[0];
     const label = document.getElementById("size-warning");
-    
+    const border = document.getElementsByClassName('tool-container')[0];
+
+    let activate = false;
+    let mode = 'black';
+
+    window.addEventListener('mousedown', () => activate = true);
+    window.addEventListener('mouseup', () => activate = false);
+
     function setLabel(message){
         gridContainer.innerHTML="";
         gridContainer.appendChild(label);//re-append label as we just cleared it with the above statement
@@ -14,24 +21,57 @@ document.addEventListener('DOMContentLoaded',() =>{
             setLabel("100x100 size grids are the maximum");
             return;
         }
-        else if(size < 0){
-            setLabel("Invalid Input")
-            return;
-        }
-        gridContainer.innerHTML = "";
-        for(let i=0; i<size*size; i++){
-            setTimeout(() => {
-                const newbox= document.createElement('div');
-                newbox.className='box';
-                newbox.style.width = (500/size) +"px";
-                newbox.style.height= (500/size) + "px";
-                newbox.addEventListener("mouseover", () => {//change color
-                    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;//a random color everytime
-                    newbox.style.backgroundColor = randomColor;
-                });
+        else if(size > 0){
+            gridContainer.innerHTML = "";
+            
+            for(let i=0; i<size*size; i++){
+                setTimeout(() => {
+                    const newbox= document.createElement('div');
+                    newbox.className='box';
+                    newbox.style.width = (500/size) +"px";
+                    newbox.style.height= (500/size) + "px"; 
+                    gridContainer.appendChild(newbox);
 
-                gridContainer.appendChild(newbox);
-            },i*0.1);
+                    //cursor control
+                    let currentcolor = '';
+                    newbox.addEventListener('mouseenter',() =>{
+                            currentcolor = newbox.style.backgroundColor;
+                            newbox.style.backgroundColor = 'brown';
+                    });
+
+                    newbox.addEventListener('mouseleave',() =>{
+                            newbox.style.backgroundColor = currentcolor;
+                        
+                    });
+
+                    newbox.addEventListener("mouseover", () => {//change color
+                        const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;//a random color everytime
+                        if(activate)
+                        {
+                            if(mode == 'black'){
+                                newbox.style.backgroundColor = 'black';
+                            }
+                            else if(mode == 'erase'){
+                                newbox.style.backgroundColor = 'lightblue';
+                            }
+                            else if(mode== 'rainbow'){
+                                newbox.style.backgroundColor = randomColor;
+                            }
+                            else{
+                                console.error("error fetching brush");
+                            }
+                        }
+                    });
+                    newbox.addEventListener("mousedown", (drag) => {
+                        drag.preventDefault(); //Prevent the default drag behavior
+                    });
+                },i*0.1);
+            }
+        }
+
+        else{
+            setLabel("invalid Input");
+            return;
         }
     }
 
@@ -42,7 +82,31 @@ document.addEventListener('DOMContentLoaded',() =>{
     const gridMakerButton = document.getElementById('grid-maker');
     const gridResetButton = document.getElementById('grid-reset');
     const gridClearButton = document.getElementById('grid-clear');
-    gridMakerButton.addEventListener('click',() => creategrid(document.getElementById('grid-size-input').value));
+    const colorBlack = document.getElementById('black-color');
+    const colorErase = document.getElementById('erase');
+    const colorRainbow = document.getElementById('rainbow-color');
+
+    colorBlack.addEventListener('click', () => {
+        mode = 'black'
+        border.style.border= '3px solid black';
+    });
+    colorRainbow.addEventListener('click', () => {
+        mode = 'rainbow';
+        border.style.border= '3px solid lawngreen';
+    });
+    colorErase.addEventListener('click', () =>{ 
+        mode = 'erase'
+        border.style.border= '3px solid royalblue';
+    });
+    
+    gridMakerButton.addEventListener('click',() => {
+        if(document.getElementById('grid-size-input').value === ""){
+            creategrid(16);
+            return;
+        }
+        creategrid(document.getElementById('grid-size-input').value);
+    });
+
     gridResetButton.addEventListener('click',() => {
         const size= document.getElementById('grid-size-input').value;
         if(!size){
@@ -56,6 +120,8 @@ document.addEventListener('DOMContentLoaded',() =>{
         }
             
     });
+
     gridClearButton.addEventListener('click',cleargrid);
+
     creategrid(16);//initial size is 16x16
 });
